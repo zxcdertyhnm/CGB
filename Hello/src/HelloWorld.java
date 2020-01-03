f80b7a80a02b5f4e0d5778b8e91879f5a4fe14b7
我的电脑 <@DESKTOP-2JONE01>
2020-01-03 11:10:23 +0800

    憨憨

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-                System.out.println("憨憨祖鸿");
+		System.out.println("憨憨");
+        System.out.println("憨憨祖鸿");
 		System.out.println("睁眼看世界");
 	}
 
